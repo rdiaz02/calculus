b24d9e854e48a2172c34ab05df4f61caa413d4cf
Ben Crowell <notreal@fakewillnotwork>
2013-01-30 12:37:27 -0800

    bug in script

--- scripts/preflight_figs.pl
+++ scripts/preflight_figs.pl
@@ -7,7 +7,7 @@ use strict;
 #   qpdf (ubuntu package qpdf)
 #   pdffonts (ubuntu package poppler-utils)
 
-my $expected_files = 1100;
+my $expected_files = 50;
 my @files = <*/figs/*.svg>;
 
 my $count_svg = 0;

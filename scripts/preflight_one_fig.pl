f53fbe2218227fc26c1e36679c357f738e6e9d0c
Ben Crowell <notreal@fakewillnotwork>
2013-01-30 12:30:44 -0800

    update scripts

--- scripts/preflight_one_fig.pl
+++ scripts/preflight_one_fig.pl
@@ -5,12 +5,16 @@ use strict;
 # usage:
 #   preflight_one_fig.pl foo.svg
 # Checks whether there is no rendered version of foo.svg.
+# Checks whether it was rendered in more than one version.
 # Checks whether it was rendered to foo.png. If so, complains if it has transparency.
 # Checks whether there is also a foo.pdf. If there is, checks foo.svg and foo.pdf for problems:
 #   transparency (checked for in the svg)
 #   fonts embedded in pdf
 #   bad pdf structure
 #   pdf older than svg
+#   pdf version is not 1.4
+# There is a facility for running each pdf through pdftk, but I currently have it
+# turned off because it's slow and doesn't seem to actually find any problems.
 # If there's a problem, prints a message to stdout and exits with nonzero error code.
 
 # requires the following tools:
@@ -23,6 +27,20 @@ my $svg = $ARGV[0];
 
 my $pdf = $svg;
 $pdf =~ s/\.svg$/.pdf/;
+
+# We want it to be rendered into exactly one format.
+my @formats = ();
+foreach my $fmt('pdf','png','jpg') {
+  my $rendered = $svg;
+  $rendered =~ s/\.svg$/.$fmt/;
+  push @formats,$fmt if -e $rendered;
+}
+if (@formats==0) {err("not rendered into pdf, png, or jpg")}
+if (@formats>1) {
+  # foreach my $fmt(@formats) {  my $rendered = $svg;  $rendered =~ s/\.svg$/.$fmt/; unlink($rendered)}
+  err("rendered into more than one format: ".join(',',@formats));
+}
+
 if (-e $pdf) {
   my $err = check_pdf($svg,$pdf);
   if ($err) {err($err)}
@@ -38,8 +56,8 @@ else {
         my $f = `identify -format '%[channels]' $bitmap`;
         if ($f=~/rgba/) {
           my $c = "mogrify -background white -flatten -alpha off $bitmap";
-          # system($c);
-          err("file $bitmap contains transparency; fix with\n  $c\nand then check visually");
+          print "removing transparency from file $bitmap\n";
+          system($c)==0 or err("error executing command $c");;
         }
       }
       exit(0);
@@ -50,7 +68,7 @@ else {
 
 sub err {
   my $message = shift;
-  print $message,"\n";
+  print "preflight_one_fig.pl, svg file $svg, ",$message,"\n";
   exit(-1);
 }
 
@@ -58,12 +76,35 @@ sub check_pdf {
   my ($svg,$pdf) = @_;
   my $err = check_for_stale_pdf($svg,$pdf);
   return $err if $err;
+  my $err = check_pdf_version($svg,$pdf);
+  return $err if $err;
   my $err = check_pdf_for_fonts($svg,$pdf);
   return $err if $err;
   my $err = check_pdf_for_transparency($svg,$pdf);
   return $err if $err;
   my $err = check_pdf_for_structure($svg,$pdf);
   return $err if $err;
+  if (0) { # slow and doesn't seem to catch any errors
+    my $err = check_pdf_in_pdftk($svg,$pdf);
+    return $err if $err;
+  }
+  return undef;
+}
+
+# This is slow and doesn't seem to catch any errors, so I'm
+# not currently using it.
+sub check_pdf_in_pdftk {
+  my ($svg,$pdf) = @_;
+  if (system("pdftk $pdf cat output /dev/null 2>/dev/null")!=0) {
+    return "pdftk error on file $pdf";
+  }
+  return undef;
+}
+
+sub check_pdf_version {
+  my ($svg,$pdf) = @_;
+  my $pdf_version = detect_pdf_version($pdf);
+  $pdf_version eq '1.4' or return "pdf version='$pdf_version'; see https://bugs.launchpad.net/inkscape/+bug/1110549";
   return undef;
 }
 
@@ -90,6 +131,9 @@ sub check_pdf_for_fonts {
   return undef;
 }
 
+# This actually involves checking the svg, not the pdf (because it's easier). We only
+# come here if it's been rendered to a pdf. If it fails this check, it needs to be
+# rendered as a bitmap.
 sub check_pdf_for_transparency {
   my ($svg,$pdf) = @_;
 
@@ -97,7 +141,7 @@ sub check_pdf_for_transparency {
   return undef unless `grep -e "opacity:[^1]" $svg`;
 
   # Now do a more reliable check.
-  # There are three types of opacity: fill-opacity, stroke-opacity, and opacity (applied to whole groups).
+  # There are at least four types of opacity: stop-opacity, fill-opacity, stroke-opacity, and opacity (applied to whole groups).
   my $transp = `xml_grep --cond='*[\@style]' $svg  | grep -e "opacity:[^1]"`;
   if ($transp ne '') {
     # Often we get something like this:
@@ -112,7 +156,7 @@ sub check_pdf_for_transparency {
       if (defined $styles{'opacity'} && $styles{"opacity"}<1) {
         return report_transparency($svg,$style);
       }
-      foreach my $sf('stroke','fill') {
+      foreach my $sf('stop','stroke','fill') {
         if (   $styles{$sf} ne 'none' # works correctly if undef
             && defined $styles{"${sf}-opacity"}
             && $styles{"${sf}-opacity"}<1 ) { 
@@ -128,5 +172,16 @@ sub report_transparency {
   my ($svg,$style) = @_;
   $style =~ /(.{0,22}opacity:[^1].{0,10})/;
   my $shorter = $1;
-  return "transparency found in file $svg : ...$shorter...";
+  return "transparency found in file $svg, so it should have been rendered as a bitmap, not a pdf : ...$shorter...";
+}
+
+# code duplicated in preflight_one_fig.pl
+sub detect_pdf_version {
+  my $pdf = shift;
+  open (my $F,"<$pdf") or return undef;
+  my $version_line = <$F>; # should be %PDF-1.4
+  close $F;
+  $version_line =~ /^\%PDF\-(\d+\.\d+)/i or return undef;
+  my $version = $1;
+  return $version;
 }
--- scripts/render_one_figure.pl
+++ scripts/render_one_figure.pl
@@ -34,6 +34,11 @@ unless (-e $pdf && -M $svg > -M $pdf) { #
   unless ($not_for_real) {
     system($c)==0 or die "error in render_one_figure.pl, rendering figure using command $c";
   }
+  # Check that inkscape output pdf 1.4, since pdftk has buggy support for 1.5.
+  # See https://bugs.launchpad.net/inkscape/+bug/1110549
+  # Don't just depend on preflight_one_fig.pl to do this, because that would result in it being rendered to bitmap.
+  my $pdf_version = detect_pdf_version($pdf);
+  $pdf_version eq '1.4' or die "error in render_one_figure.pl, inkscape output pdf version='$pdf_version'; see https://bugs.launchpad.net/inkscape/+bug/1110549";
 }
 
 -x "scripts/preflight_one_fig.pl" or die "couldn't find scripts/preflight_one_fig.pl -- are you running me from home dir?";
@@ -41,13 +46,25 @@ unless (-e $pdf && -M $svg > -M $pdf) { #
 if (system("scripts/preflight_one_fig.pl $svg")==0) {exit(0)}
 
 print "  preflight failed on pdf rendering of $svg , probably due to transparency; rendering to bitmap instead\n";
-unlink($pdf);
+unlink($pdf) or die "error deleting $pdf, $!";
 my $png = $svg;
 $png=~s/\.svg$/.png/;
 my $c="inkscape --export-png=$png --export-dpi=300 $svg  --export-area-drawing  1>/dev/null"; 
 print "  $c\n"; 
 unless ($not_for_real) {
   system($c)==0 or die "error in render_one_figure.pl, rendering figure using command $c";
+  system("mogrify -background white -flatten -alpha off $png")==0 or die "error removing transparency from $png";
 }
 
 print "\n";
+
+# code duplicated in preflight_one_fig.pl
+sub detect_pdf_version {
+  my $pdf = shift;
+  open (my $F,"<$pdf") or return undef;
+  my $version_line = <$F>; # should be %PDF-1.4
+  close $F;
+  $version_line =~ /^\%PDF\-(\d+\.\d+)/i or return undef;
+  my $version = $1;
+  return $version;
+}
